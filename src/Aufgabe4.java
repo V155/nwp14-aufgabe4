718a0b7a10ed5bc80e44bd6e358f2ea0bdf6dcfa
Fritz Reichwald <fritz.reichwald@gmail.com>
2014-07-07 17:20:51 +0200

    parsing of trpt works now

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -1,5 +1,6 @@
 package de.reichwald.fh.nwp14;
 import de.reichwald.fh.nwp14.IpPackage;
+import de.reichwald.fh.nwp14.TrptPackage;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
@@ -12,33 +13,83 @@ public class Aufgabe4
 
     public static void main(String args[])
     {
-	ArrayList<IpPackage> packageList = new ArrayList();
+	//	ArrayList<IpPackage> list1 = readTcpdump();
+	ArrayList<TrptPackage> list2 = readTrptdump();
+    }
+
+    public static ArrayList<TrptPackage> readTrptdump(){
+	ArrayList<TrptPackage> trptPackageList = new ArrayList();
+
+	try{
+	    BufferedReader br = new BufferedReader(new FileReader("trptdump.dump"));
+	    String line = "";
+	    String output = "";
+	    String seqNr = "";
+	    String rto = "";		
+
+	    while ((line = br.readLine()) != null) {
+		if(line.matches("^[0-9]{3} (ESTABLISHED:output|ESTABLISHED:input|SYN_SENT:input) .*")){
+		    output = new String(line);
+		    if(line.matches("^[0-9]{3} (ESTABLISHED:input|SYN_SENT:input) .*")){
+			int index1 = line.indexOf(')')+1;
+			seqNr = new String(line.substring(index1,index1+8));
+		    }
+
+		    if(line.matches("^[0-9]{3} (ESTABLISHED:output) .*")){
+			int index1 = line.indexOf(')')+2;
+			seqNr = new String(line.substring(index1,index1+8));
+		    }
+		    for(int i = 0; i < 4; i++){
+			if ((line = br.readLine()) != null){
+			    output = output + "\n" + line;
+			    if (i==2){
+				rto=line.substring(line.indexOf('=')+1,line.indexOf(','));
+			    }
+			}
+		    }
+		    trptPackageList.add(new TrptPackage(output, seqNr, rto));
+		}
+	    }
+	    br.close();
+	    for (TrptPackage trptpackage : trptPackageList){
+		System.out.println(trptpackage.getSeqNr() + ", " + trptpackage.getRto());
+	    }
+	}
+	catch(FileNotFoundException e){
+	}
+	catch(IOException e){
+	}
+	return new ArrayList<TrptPackage>();
+    }
+		    
+    
+    public static ArrayList<IpPackage> readTcpdump(){
+	ArrayList<IpPackage> ipPackageList = new ArrayList();
+	
 	try{
 	    BufferedReader br = new BufferedReader(new FileReader("tcpdump.dump"));
 	    String line = "";
 	    String output = "";
 	    String seqNr = "";
-	    boolean next = false;
-	    boolean first = true;
-
+	    
 	    while ((line = br.readLine()) !=null) {
 		// process the line.
 		// ^([0-9][0-9]:){2}[0-9]{2}.[0-9]{6} IP
-		if(line.matches("^([0-9][0-9]:){2}[0-9]{2}\\.[0-9]{6}.* IPv4 .*"))
+		if(line.matches("^([0-9][0-9]:){2}[0-9]{2}\\.[0-9]{6} IP .*"))
 		    {
 			output=new String(line);
-			for (int i=0; i < 3; i++)
+			for (int i=0; i < 4; i++)
 			    {
 				if ((line = br.readLine()) != null){
 				    output = output + "\n" + line;
-				    if (i == 2)
+				    if (i == 3)
 					{
-					     
-					    seqNr = new String(line.substring(42,46) + line.substring(47,51));
+					    
+					    seqNr = new String(line.substring(35,39) + line.substring(40,44));
 					}
 				}
 			    }
-			packageList.add(new IpPackage(output, seqNr, "rtt", "rto", "timestamp", "tcpdumpNr"));
+			ipPackageList.add(new IpPackage(output, seqNr, "rtt", "rto", "timestamp", "tcpdumpNr"));
 			//System.out.println(output);
 		    }
 		//else if(line.matches("^([0-9][0-9]:){2}[0-9]{2}\\.[0-9]{6}.* IPv4 .*") && next){
@@ -48,16 +99,18 @@ public class Aufgabe4
 		
 	    }
 	    br.close();
-	    for (IpPackage ippackage : packageList){
+	    for (IpPackage ippackage : ipPackageList){
 		System.out.println(ippackage.getSeqNr());
 	    }
-
+	    
 	}
 	catch(FileNotFoundException e){
 	}
 	catch(IOException e){
 	}
-	
-	
+	return new ArrayList<IpPackage>();
     }
+    
+    
 }
+
